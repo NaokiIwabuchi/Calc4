843b830f4834c1a4246942c6a99ab4cf34fb3ab0
naoki.iwabuchi <worldwideromona@gmail.com>
2019-05-04 22:04:41 +0900

    よろしくお願いします。

---
+++ app/src/main/java/iwabuchi/naoki/techacademy/calc4/SecondActivity.kt
@@ -0,0 +1,12 @@
+package iwabuchi.naoki.techacademy.calc4
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
